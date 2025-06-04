2d697cec70a239397b5d34d144aa277134a977d8
2.0FixDepotHTML <juanitarozo2@gmail.com>
2025-06-03 19:06:03 -0500

    Primer commit - estructura inicial FixDepot

--- Backend/model/Product.js
+++ Backend/model/Product.js
@@ -1,15 +1,3 @@
-const mongoose = require('mongoose');
-
-const ProductSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: String,
-  price: { type: Number, required: true },
-  stock: { type: Number, default: 0 },
-  category: String,
-  image: String,
-  provider: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  ratings: [{ userId: mongoose.Schema.Types.ObjectId, score: Number }],
-  comments: [{ userId: mongoose.Schema.Types.ObjectId, text: String, date: Date }]
-});
-
-module.exports = mongoose.model('Product', ProductSchema);
+router.get('/:id/recommendations', getRecommendations);
+router.get('/:id/comments', getComments);
+router.post('/:id/comments', authMiddleware, addComment);
